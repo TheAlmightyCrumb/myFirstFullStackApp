6acb91afb0eb41644d6bdc90ad70b19bc44926e3
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-08-04 19:20:28 +0300

    Creating a static array

--- rest.js
+++ rest.js
@@ -3,5 +3,19 @@ const app = express();
 app.use(express.json());
 
 let products = [
-    
+    {
+        id: 'Pizza',
+        author: 'Maggie',
+        notes: 'NO VEGAN CHEESE!!!'
+    },
+    {
+        id: 'Tortillas',
+        author: 'Ben',
+        notes: 'Get the burrito-sized ones'
+    },
+    {
+        id: 'Black Rum',
+        author: 'Ben',
+        notes: 'It goes well with the burrito'
+    }
 ];
